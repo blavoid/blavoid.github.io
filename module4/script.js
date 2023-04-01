c3c86c4e0d8bcc79df8f0f0cb184a29c54e88368
blavoid <ayoubredninja@gmail.com>
2023-04-01 18:36:11 +0000

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -1,11 +1,11 @@
 var names=new Array();
 names[0]="Yaakov";
 names[1]="John";
-names[2]="paul";
-names[3]="Jen";
-names[4]="frank";
-names[5]="larry";
-names[6]="jason";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
 names[7]="paula";
 names[8]="laura";
 names[9]="jim";
